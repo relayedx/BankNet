159c5ceff4b9b16203166fffef1e607a7aeea333
Jaoa0204 <jerrickalone@gmail.com>
2026-04-14 16:22:31 -0700

    Added attributes, and getters and setters to LoginMessage

--- Phase 3 Files/LoginMessage.java
+++ Phase 3 Files/LoginMessage.java
@@ -1,6 +1,18 @@
 
 public class LoginMessage extends Message{
-	public LoginMessage(msgType type, Status status) {
+	private final String user;
+	private final String pass;
+	public LoginMessage(msgType type, Status status, String user, String pass) {
 		super(type,status);
+		this.user = user;
+		this.pass = pass;
+	}
+	
+	public String getUser() {
+		return user;
+	}
+	
+	public String getPass() {
+		return pass;
 	}
 }
