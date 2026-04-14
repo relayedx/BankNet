5e0391ae97659481153b27eae06be5f4ba3c1d88
Jaoa0204 <jerrickalone@gmail.com>
2026-04-14 16:25:28 -0700

    edited testdriver to work

--- Phase 3 Files/MsgTest.java
+++ Phase 3 Files/MsgTest.java
@@ -1,6 +1,6 @@
 
 public class MsgTest {
-	public static void main() {
+	public static void main(String[] args) {
 		LoginMessage msg = new LoginMessage(msgType.LOGIN_REQUEST,Status.IN_PROGRESS,"test","pass");
 		System.out.println(msg.getUser());
 	}
