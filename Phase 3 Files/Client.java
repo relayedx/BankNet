c45d984e363de8d1892a758972a8fc8bdd2cfc69
Jaoa0204 <jerrickalone@gmail.com>
2026-04-14 17:24:34 -0700

    Add ClientController and Login function
    from UML diagram

--- Phase 3 Files/Client.java
+++ Phase 3 Files/Client.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Client {
-
+	static ClientController controller;
 	static ObjectOutputStream out; 
 	static ObjectInputStream in;
     public static void main(String[] args) throws IOException, ClassNotFoundException {
@@ -17,6 +17,7 @@ public class Client {
         String host = sc.next();
         
         Client ref = new Client(); 
+        controller = new ClientController(ref);
         // Connect to the ServerSocket at host:port
         Socket socket = new Socket(host, port);
         System.out.println("Connected to " + host + ":" + port);
@@ -25,25 +26,44 @@ public class Client {
         OutputStream outputStream = socket.getOutputStream();
 
         // Create object output stream from the output stream to send an object through it
-        ObjectOutputStream out = new ObjectOutputStream(outputStream);
+        out = new ObjectOutputStream(outputStream);
         // Create a input stream to read these objects as well
-        ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+        in = new ObjectInputStream(socket.getInputStream());
 
+        
+        // TODO: Call for login GUI here (Fosa)
+        
+        /// SIMULATED LOGIN
+        controller.login("test", "test"); // This will be called from GUI
+        
+        // 
         // List of Message objects
         //List<Message> messages = new ArrayList<>();
        // messages.add(new Message("This is a test message!"));
 
 
-        System.out.println("Sending Message Object");
-        out.writeObject(new LoginMessage(msgType.LOGIN_REQUEST, Status.IN_PROGRESS, "user","pass"));
-        
-		Message msg = (Message) in.readObject();
-		System.out.println(msg.getStatus() + " " + msg.getType());
+
 
         
         
         System.out.println("Closing socket");
         socket.close();
+    }
+    
+    public boolean login(String user, String pass) throws IOException, ClassNotFoundException { // Called from ClientController
+   
+        System.out.println("Sending LoginMessage Object");
+        out.writeObject(new LoginMessage(msgType.LOGIN_REQUEST, Status.IN_PROGRESS, "user","pass")); // Client makes msg to send to the server
+        out.flush(); // Sends the message
+		Message msg = (Message) in.readObject(); // We are expecting a message back
+		if (msg.getType() != msgType.LOGIN_REQUEST) { // If the server sends back anything else other than a login_request
+			return false; // Something is wrong, return false.
+		}
+		if (msg.getStatus() == Status.SUCCESS) { // If the login_request is true
+			return true;
+		}else {
+			return false;
+		}
     }
 }
 
---
+++ Phase 3 Files/ClientController.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+
+import javax.swing.JOptionPane;
+
+public class ClientController {
+	private Client client;
+	public ClientController(Client client){
+		this.client = client; 
+	}
+	
+	/// METHODS CALLED FROM GUI
+	public void login(String user, String pass) throws ClassNotFoundException, IOException { // This method is called from GUI
+		// We call client login with the arguments we are given
+		System.out.println("Calling to see if " + user + pass + " is authenticatdd");
+		boolean auth = client.login(user, pass); // See if the client is an auth user
+		// Request for accounts
+		// TODO: Display ATM GUI or Teller GUI (Fosa)
+		if (auth) {
+			JOptionPane.showMessageDialog(null, "User is a user"); // TODO: These will be replaced w/ gui, placeholder for debugging
+		}else {
+			JOptionPane.showMessageDialog(null, "Error");
+		}
+	}
+}
