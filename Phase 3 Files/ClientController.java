2d76aa1b03fc32a51e9ade6f8a2befc6f6f3809f
fosa <obaseki321@gmail.com>
2026-04-18 16:34:01 -0700

    slight paramater/var name changes

--- Phase 3 Files/ClientController.java
+++ Phase 3 Files/ClientController.java
@@ -14,9 +14,9 @@ public class ClientController {
 	}
 	
 	/// METHODS CALLED FROM GUI
-	public void login(String user, String pass) throws ClassNotFoundException, IOException { // This method is called from GUI
+	public void login(String username, String password) throws ClassNotFoundException, IOException { // This method is called from GUI
 		// We call client login with the arguments we are given
-		boolean auth = client.login(user, pass); // See if the client is an auth user
+		boolean auth = client.login(username, password); // See if the client is an auth user
 		// Request for accounts
 		// TODO: Display ATM GUI or Teller GUI (Fosa)
 		if (auth) {
