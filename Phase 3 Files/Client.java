e7af26474e6f22b9d3e27b62e1493e7bdf22063d
fosa <obaseki321@gmail.com>
2026-04-18 13:15:14 -0700

    restructured client connection and created blocking to stop main termination after GUI launch

--- Phase 3 Files/Client.java
+++ Phase 3 Files/Client.java
@@ -1,57 +1,77 @@
-
 import java.io.*;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.concurrent.CountDownLatch;
+
 
 public class Client {
-	static ClientController controller;
-	static ObjectOutputStream out; 
-	static ObjectInputStream in;
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
+	private Socket socket;
+	private ObjectOutputStream out; 
+	private ObjectInputStream in;
+	private CountDownLatch latch;
+	
+	public Client(Socket socket, CountDownLatch latch) throws IOException {
+		this.latch = latch;
+		try {
+			this.socket = socket;
+			this.out = new ObjectOutputStream(socket.getOutputStream());
+			this.in = new ObjectInputStream(socket.getInputStream());
+		} catch (Exception e) {
+			System.out.print("Error: " + e);
+		}
+	}
+	
+	public void disconnect() throws IOException {
+		socket.close();
+		latch.countDown();
+	}
+	
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+    	// creating latch counter to stop main from terminating 
+    	CountDownLatch latch = new CountDownLatch(1);
+    	
         Scanner sc= new Scanner(System.in); //System.in is a standard input stream.
-        System.out.print("Enter the port number to connect to: <1234>");
+        System.out.println("Enter the port number to connect to: <1234>");
         int port = sc.nextInt();
-        System.out.print("Enter the host address to connect to: <localhost> ");
+        System.out.println("Enter the host address to connect to: <localhost> ");
         String host = sc.nextLine();
         
         // Connect to the ServerSocket at host:port
         Socket socket = new Socket(host, port);
         System.out.println("Connected to " + host + ":" + port);
-
-        // instantiation of client and clientController
-        // once a clientController is instantiated it will call LoginGUI on its own!
-        Client ref = new Client(); 
-        controller = new ClientController(ref);
-        
  
+        Client clientRef = new Client(socket, latch);
+        ClientController controller = new ClientController(clientRef);
+        latch.await();
         // Output stream socket.
-        OutputStream outputStream = socket.getOutputStream();
+        //OutputStream outputStream = socket.getOutputStream();
 
         // Create object output stream from the output stream to send an object through it
-        out = new ObjectOutputStream(outputStream);
+        //out = new ObjectOutputStream(outputStream);
         // Create a input stream to read these objects as well
-        in = new ObjectInputStream(socket.getInputStream());
+        //in = new ObjectInputStream(socket.getInputStream());
+        
+        // instantiation of client and clientController
+        // once a clientController is instantiated it will call LoginGUI on its own!
+        //Client ref = new Client();
+        //controller = new ClientController(ref);
         
         /// SIMULATED LOGIN
+        /*
         controller.login("test", "test"); // This will be called from GUI
         controller.withdraw(1, 10, "User");
         controller.resetPassword("test", "test");
         controller.deposit(1, 120, "user");
         controller.getSkelAccts("user");
-        // 
+        */
         // List of Message objects
         //List<Message> messages = new ArrayList<>();
        // messages.add(new Message("This is a test message!"));
-
-
-
-
         
-        
-        System.out.println("Closing socket");
-        socket.close();
+        //System.out.println("Closing socket");
+        //socket.close();
     }
     
     public boolean login(String user, String pass) throws IOException, ClassNotFoundException { // Called from ClientController
