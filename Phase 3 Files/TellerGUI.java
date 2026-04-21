424e4a4b8bce31430e1b135b2d6394a14c24c19b
fosa <obaseki321@gmail.com>
2026-04-20 20:38:52 -0700

    added initial TellerGUI setup

---
+++ Phase 3 Files/TellerGUI.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+class TellerGUI implements RoleBasedGUI {
+	private ClientController clientController;
+	private JFrame frame;
+	
+	public TellerGUI(ClientController clientController) {
+		this.clientController = clientController;
+		
+	}
+	
+	public void launchUI() {
+		
+	}
+	
+	public void closeUI() {
+		
+	}
+}
