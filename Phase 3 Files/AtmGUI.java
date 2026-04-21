8590bf181e54c0a365d7f5f13081ea441ec555b0
fosa <obaseki321@gmail.com>
2026-04-20 20:30:44 -0700

    created ATM GUI frame to run role based test

--- Phase 3 Files/AtmGUI.java
+++ Phase 3 Files/AtmGUI.java
@@ -1,14 +1,32 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
 public class AtmGUI implements RoleBasedGUI {
-
+	private JFrame frame;
+	private ClientController clientController;
 	
-	
+	public AtmGUI(ClientController clientController) {
+		this.clientController = clientController;
+	}
 	
 	public void launchUI() {
+		// GUI frame
+		frame = new JFrame("Welcome Customer!");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(1050, 750);
+		frame.setLocationRelativeTo(null);
+		frame.setResizable(false);
 		
+		
+		
+		
+		frame.setVisible(true);
 	}
 	
 	public void closeUI() {
-		
+		if (frame != null) {
+			frame.dispose();
+		}
 	}
 }
