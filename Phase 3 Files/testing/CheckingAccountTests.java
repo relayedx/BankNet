12859047e7054f0eebb1167a5cd2e07723e91a4e
Jaoa0204 <jerrickalone@gmail.com>
2026-04-27 19:28:45 -0700

    Initialize testing for Checking Accounts

---
+++ Phase 3 Files/testing/CheckingAccountTests.java
@@ -0,0 +1,60 @@
+package testing;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import dev.BankAcct;
+import dev.TranType;
+import dev.Transaction;
+import dev.User;
+import dev.AcctType;
+
+
+public class CheckingAccountTests {
+	private BankAcct testAcct;
+	
+	@BeforeEach
+	public void setUp() {
+		List<Integer> authAccts = new ArrayList<>();
+		authAccts.add(1);
+		authAccts.add(2);
+		User user = new User("jerrick", "pass", false,authAccts, true);
+		testAcct = new BankAcct(AcctType.Checking,user);
+		Transaction trans1 = new Transaction("User", 100, TranType.DEPOSIT);
+		testAcct.deposit(trans1);
+	}
+	
+	@Test
+	@DisplayName("Constructor Test")
+	public void constructorTest() {
+		Assertions.assertNotNull(testAcct);
+	}
+	
+	@Test
+	@DisplayName("Deposit Test")
+	public void depositTest() {
+		Transaction trans1 = new Transaction("User", 10, TranType.DEPOSIT);
+		testAcct.deposit(trans1);
+		Assertions.assertTrue(testAcct.getBalance() > 100);
+	}
+	
+	@Test
+	@DisplayName("Withdrawal Test")
+	public void withdrawTest() {
+		Transaction trans1 = new Transaction("User", 10, TranType.WITHDRAWAL);
+		testAcct.withdraw(trans1);
+		Assertions.assertTrue(testAcct.getBalance() < 100);
+	}
+	
+	@Test
+	@DisplayName("Withdrawal Overdraft Test")
+	public void overdraft() {
+		Transaction trans1 = new Transaction("User", 110, TranType.WITHDRAWAL);
+		Assertions.assertFalse(testAcct.withdraw(trans1));
+	}
+
+}
