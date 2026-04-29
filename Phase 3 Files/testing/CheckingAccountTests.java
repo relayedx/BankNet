391fd1fbeb8cbc0a0bce0036a9ccde2f67f62958
Jaoa0204 <jerrickalone@gmail.com>
2026-04-28 20:59:52 -0700

    Added rest of test cases for checking account

--- Phase 3 Files/testing/CheckingAccountTests.java
+++ Phase 3 Files/testing/CheckingAccountTests.java
@@ -93,8 +93,77 @@ public class CheckingAccountTests {
 		TransactionMessage msg = testAcct.withdraw(trans1);
 		Assertions.assertEquals(Status.ERROR,msg.getStatus());
 	}
-
 	
+	@Test
+	@DisplayName("Credit Account Attributes Test")
+	public void creditNull() { // Tests whether or not a credit was initialized (should be none)
+		Assertions.assertAll(
+				() -> Assertions.assertNull(testAcct.getDueDate()),
+				() -> Assertions.assertEquals(0, testAcct.getAvailCredit()),
+				() -> Assertions.assertEquals(0, testAcct.getCredit())
+		);
+	}
+
+	// The below tests will not be tested in the other types of bank accounts, since the logic will not change //
+	@Test
+	@DisplayName("Adding Authorized User Test")
+	public void addAuth() {
+		User user = new User("hii", "pass", null, false, null, true);
+		testAcct.addAuthUser(user);
+		Assertions.assertTrue(testAcct.getAuths().contains(user));
+	}
+	
+	@Test
+	@DisplayName("Adding Repeat Authorized User Test")
+	public void addAuthRepeat() {
+		User user = new User("jerrick", "pass", null, false, null, true);
+		testAcct.addAuthUser(user);
+		Assertions.assertEquals(1, testAcct.getAuths().size());
+	}
+	
+	@Test
+	@DisplayName("Removing Authorized User Test")
+	public void delAuth() {
+		User user = new User("jerrick", "pass", null, false, null, true);
+		testAcct.removeAuth(user);
+		Assertions.assertFalse(testAcct.getAuths().contains(user));
+	}
+	
+	@Test
+	@DisplayName("Removing NonExistent Authorized User Test")
+	public void delAuthNE() {
+		User user = new User("notthere", "pass", null, false, null, true);
+		testAcct.removeAuth(user);
+		Assertions.assertEquals(1,testAcct.getAuths().size());
+	}
+	
+	@Test
+	@DisplayName("Closing Account")
+	public void closeAcct() {
+		Assertions.assertTrue(testAcct.closeAcc());
+	}
+	
+	@Test
+	@DisplayName("Closing an already closed Account")
+	public void closeAcctError() {
+		testAcct.closeAcc(); // Close the account 
+		Assertions.assertFalse(testAcct.closeAcc()); // See if it lets us close it again
+	}
+	
+	@Test
+	@DisplayName("Freezing Account")
+	public void freezeAcct() {
+		testAcct.freezeAcc();
+		Assertions.assertTrue(testAcct.getFrozen());
+	}
+	
+	@Test
+	@DisplayName("Unfreezing Account")
+	public void unfreezeAcct() {
+		testAcct.freezeAcc(); // Freeze it
+		testAcct.freezeAcc(); // Unfreeze it
+		Assertions.assertFalse(testAcct.getFrozen()); // Is it unfrozen?
+	}
 	
 
 }
